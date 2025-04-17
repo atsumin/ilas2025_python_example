5dbbdd2eec0306c66604ac5bf807916c818538ad
Noritoshi Atsumi <atsumi.noritoshi.5u@kyoto-u.ac.jp>
2025-04-17 16:44:36 +0900

    add comment.py

---
+++ comment.py
@@ -0,0 +1,5 @@
+# This program helps travelers use elevators
+jf_str = input('Enter the Japanese Floor Number: ')
+jf = int(jf_str)
+ef = jf - 1 # The conversion is quite simple
+print('Non-Japanese Floor Number is', ef)
